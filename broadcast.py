775e8f0badcf89bb883ec32ad6a6ed73ad11dba4
Davy Vanherbergen <davy.vanherbergen@sc02sx0efgtdx.local>
2017-04-27 12:00:55 +0200

    fix

--- broadcast.py
+++ broadcast.py
@@ -20,16 +20,16 @@ def get_ip_address(ifname):
 
 
 # For certificate based connection
-myMQTTClient = AWSIoTMQTTClient("colruyt-pi")
-myMQTTClient.configureEndpoint("a31v8wvq1ft4tj.iot.eu-west-2.amazonaws.com", 8883)
-myMQTTClient.configureCredentials("/home/pi/certs/aws-root-ca.cert", "/home/pi/certs/colruyt-pi-private.key", "/home/pi/certs/colruyt-pi.cert")
+client = AWSIoTMQTTClient("colruyt-pi")
+client.configureEndpoint("a31v8wvq1ft4tj.iot.eu-west-2.amazonaws.com", 8883)
+client.configureCredentials("/home/pi/certs/aws-root-ca.cert", "/home/pi/certs/colruyt-pi-private.key", "/home/pi/certs/colruyt-pi.cert")
 
-myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+client.configureDrainingFrequency(2)  # Draining: 2 Hz
+client.configureConnectDisconnectTimeout(10)  # 10 sec
+client.configureMQTTOperationTimeout(5)  # 5 sec
 
-myMQTTClient.connect()
+client.connect()
 
 def main_loop():
 	print("loop")
@@ -37,10 +37,10 @@ def main_loop():
 	#wlan0_ip = get_ip_address("wlan0")
 	#print("eth0  : %s" % eth0_ip)
 	#print("wlan0 : %s" % wlan0_ip)
-	#myMQTTClient.publish("colruyt-pi/ip/eth0", eth0_ip, 0)
+	#client.publish("colruyt-pi/ip/eth0", eth0_ip, 0)
 	moist, temp = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
-	myMQTTClient.publish("colruyt-pi/moist",moist, 0)
-	myMQTTClient.publish("colruyt-pi/temp",temp,0)
+	client.publish("colruyt-pi/moist",moist, 0)
+	client.publish("colruyt-pi/temp",temp,0)
 
 
 
@@ -49,4 +49,4 @@ while True:
 	time.sleep(5)
 
 
-myMQTTClient.disconnect()
+client.disconnect()
