f0fff2f997f6d6bc66c2b16af773455512986af1
Davy Vanherbergen <davy.vanherbergen@sc02sx0efgtdx.local>
2017-04-27 11:21:37 +0200

    update

--- broadcast.py
+++ broadcast.py
@@ -2,10 +2,13 @@
 import socket
 import fcntl
 import struct
+import time
+
 
 # Import SDK packages
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
+starttime=time.time()
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -20,24 +23,27 @@ def get_ip_address(ifname):
 # For certificate based connection
 myMQTTClient = AWSIoTMQTTClient("colruyt-pi")
 myMQTTClient.configureEndpoint("a31v8wvq1ft4tj.iot.eu-west-2.amazonaws.com", 8883)
-myMQTTClient.configureCredentials("aws-root-ca.cert", "colruyt-pi-private.key", "colruyt-pi.cert")
-# For Websocket, we only need to configure the root CA
-# myMQTTClient.configureCredentials("YOUR/ROOT/CA/PATH")
+myMQTTClient.configureCredentials("/home/pi/certs/aws-root-ca.cert", "/home/pi/certs/colruyt-pi-private.key", "/home/pi/certs/colruyt-pi.cert")
+
 myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
-
-
-eth0_ip = get_ip_address("eth0")
-wlan0_ip = get_ip_address("wlan0")
-
-
-print("eth0  : %s" % eth0_ip)
-print("wlan0 : %s" % wlan0_ip)
-
 myMQTTClient.connect()
-myMQTTClient.publish("colruyt-pi/ip/eth0", eth0_ip, 0)
-myMQTTClient.publish("colruyt-pi/ip/wlan0", wlan0_ip, 0)
+
+def main_loop():
+	eth0_ip = get_ip_address("eth0")
+	wlan0_ip = get_ip_address("wlan0")
+	print("eth0  : %s" % eth0_ip)
+	print("wlan0 : %s" % wlan0_ip)
+	myMQTTClient.publish("colruyt-pi/ip/eth0", eth0_ip, 0)
+	myMQTTClient.publish("colruyt-pi/ip/wlan0", wlan0_ip, 0)
+
+
+while True:
+  main_loop()
+  time.sleep(60.0 - ((time.time() - starttime) % 5.0))
+
+
 myMQTTClient.disconnect()
