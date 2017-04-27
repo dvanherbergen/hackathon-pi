f3352a1102ffb3225e82067d405dd98c5cc34a82
Davy Vanherbergen <davy.vanherbergen@sc02sx0efgtdx.local>
2017-04-27 20:05:38 +0200

    update

--- broadcast.py
+++ broadcast.py
@@ -4,7 +4,9 @@ import fcntl
 import struct
 import time
 import Adafruit_DHT 
+import minimalmodbus
 import json
+
 
 # Import SDK packages
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
@@ -17,6 +19,74 @@ def get_ip_address(ifname):
         0x8915,  # SIOCGIFADDR
         struct.pack('256s', ifname[:15])
     )[20:24])
+
+
+
+m = minimalmodbus
+
+# Baud Rate : 19200
+# MODBUS RTU format : 1-8-N-1
+
+# Data bits 8
+m.BYTESIZE = 8
+# Parity none
+m.PARITY = 'N'
+# Stop bits 2
+m.STOPBITS = 1
+m.TIMEOUT = 0.5 
+
+# 38400 baud
+m.BAUDRATE = 19200
+
+i = m.Instrument('/dev/tty.usbserial-AK04OVSN', 1)
+
+
+
+override_control = {
+  0: "None",
+  1: "Open Sequence 1 (0%)",
+  2: "Open Sequence 2 (100%)",
+  3: "Close (50%)",
+  4: "Vmax Sequence 1",
+  5: "Vmax Sequence 2"
+}
+
+actuator_type = {
+  0: "Unknown",
+  1: "Air & Water",
+  2: "EPIV/VAV",
+  3: "Fire",
+  4: "EnergyValve",
+  5: "6-way EPIV"
+}
+
+active_sequence = {
+  0: "Sequence 1 (0...33%)",
+  1: "Sequence 2 (67...100%)",
+  2: "Dead band (34...66%)"
+}
+
+control_mode = {
+  0: "Position Control",
+  1: "Flow Control"
+}
+
+unit_selection_flow = {
+  0: "m3/s",
+  1: "m3/h",
+  2: "l/s",
+  3: "l/min",
+  4: "l/h",
+  5: "gpm",
+  6: "cfm"
+}
+
+setpoint_source = {
+  0: "Analog",
+  1: "Bus"
+}
+
+
 
 
 
@@ -40,14 +110,39 @@ def main_loop():
 	#print("wlan0 : %s" % wlan0_ip)
 	#client.publish("colruyt-pi/ip/eth0", eth0_ip, 0)
   moist, temp = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
-  data = {"device_id":"colruyt-pi", "timestamp": time.time(), "moisture": moist, "temperature": temp}
+
+  data = {}
+  data['device_id'] = "colruyt-pi"
+
+  data['moisture'] = moist
+  data['temperature'] = temp
+  data['recorded_on'] = time.time()
+"""
+  data['setpoint'] = i.read_register(0, 2)
+  data['override_control'] = override_control.get(i.read_register(1, 0), "unknown")
+  data['actuator_type'] = actuator_type.get(i.read_register(3, 0), "unknown")
+  data['relative_pos'] = i.read_register(4, 2)
+  data['absolute_pos'] = i.read_register(5, 2)
+  data['relative_flow'] = i.read_register(6, 2)
+  data['absolute_flow'] = i.read_long(9, 3)
+  data['setpoint_absolute_flow'] = i.read_long(13, 3)
+  data['setpoint_analog'] = i.read_register(15, 2)
+  data['active_sequence'] = active_sequence.get(i.read_register(16, 0), "unknown")
+  data['serial'] = str(i.read_register(100, 0)) + "-" + str(i.read_register(101, 0)) + "-" + str(i.read_register(102, 0))
+  data['firmware'] = i.read_register(103, 0)
+  data['malfunction'] = i.read_register(104, 0)
+  data['vmax_1'] = i.read_register(105, 2)
+  data['vmax_2'] = i.read_register(106, 2)
+  data['control_mode'] = control_mode.get(i.read_register(116, 0), "unknown")
+  data['unit_selection_flow'] = unit_selection_flow.get(i.read_register(117, 0), "unknown")
+  data['setpoint_source'] = setpoint_source.get(i.read_register(118, 0), "unknown")
   json_data = json.dumps(data)
+"""
   client.publish("colruyt-pi", json_data, 0)
   print(json_data)
 
 while True:
 	main_loop()
 	time.sleep(5)
-
 
 client.disconnect()
